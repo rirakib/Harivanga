14a61a532fa2ec8e9a532b53b04163801a5d4f24
Md. Rakibul Islam <83156839+rirakib@users.noreply.github.com>
2021-06-20 14:11:17 +0600

    Harivanga

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// THIS IS FOR JAVASCRIPT PAGE
+
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        items:1,
+        autoPlay:true,
+        autoPlaySpeed:1000
+    })
+
+});
